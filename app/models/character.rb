e269b5114ad61868eaedfd19e051b8be187f3e1b
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-07-12 13:39:27 -0500

    Added validation to Character and Publisher model.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,5 @@
 class Character < ApplicationRecord
   belongs_to :publisher
+
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,3 +1,5 @@
 class Publisher < ApplicationRecord
     has_many :characters
+
+    validates :name, presence: true, uniqueness: true
 end
